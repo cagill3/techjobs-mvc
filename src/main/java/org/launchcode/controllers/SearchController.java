252abc1b1a2783edad0c69a2e01e9111e02eada5
carlgillis <carldgillis@gmail.com>
2018-06-03 11:49:20 -0500

    Added search results handler

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,10 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "/search/results")
+    public String search(Model model, @RequestParam String searchType , @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices );
+        return "search";
 
+    }
 }
